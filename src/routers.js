9cc864bfe492d69cfa961e1ef66b2bff2e8d9801
wangqi04 <wangqi04@bianfeng.com>
2017-04-22 19:40:11 +0800

    更新

--- src/routers.js
+++ src/routers.js
@@ -1,10 +1,6 @@
 // 欢迎页
 const Home = r => require.ensure([], () => r(require('./view/home.vue')), 'group-home');
 
-// 用户管理
-// const user_single= r => require.ensure([], () => r(require('./view/user/single/index.vue')), 'group-user');
-// const user_batch= r => require.ensure([], () => r(require('./view/user/batch/index.vue')), 'group-user');
-
 var routes = [
   {
     path: '/',
