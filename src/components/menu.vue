a6c68e4ebc0b87b744ab5a2668e396b8ce4c9f33
王琪04 <wangqi04@gameabc.corp>
2017-05-10 20:40:55 +0800

    前端瞎更新

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -3,20 +3,47 @@
     <!-- <el-col :span="8"> -->
       <!-- <h5>带 icon</h5> -->
       <div class="menu">
-        <h2>xx,你好</h2>
+        <div class="user-info">
+          你好,
+          <h3>喻文老师</h3>
+          <el-button type="text">退出登录</el-button>
+        </div>
+
         <el-menu default-active="1-1" class="el-menu-vertical-demo" :router="true">
           <el-submenu index="1">
-            <template slot="title"><i class="el-icon-message"></i>导航一</template>
+            <template slot="title">学生管理</template>
             <el-menu-item-group>
-              <template slot="title">教师管理</template>
-            <el-menu-item index="1-1" :route="{path: '/home'}">选项1</el-menu-item>
-              <el-menu-item index="1-2" :route="{path: '/teacher'}">列表页</el-menu-item>
+              <!-- <template slot="title">学生管理</template> -->
+            <el-menu-item index="1-1" :route="{path: '/home'}">导入学生信息</el-menu-item>
+              <el-menu-item index="1-2" :route="{path: '/teacher'}">学生列表</el-menu-item>
             </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">实习管理</template>
+            <el-menu-item-group>
+              <template slot="title">学生管理</template>
+            <el-menu-item index="2-1" :route="{path: '/home'}">导入学生信息</el-menu-item>
+              <el-menu-item index="2-2" :route="{path: '/teacher'}">学生列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">校企合作</template>
+            <el-menu-item-group>
+              <template slot="title">学生管理</template>
+            <el-menu-item index="3-1" :route="{path: '/home'}">导入学生信息</el-menu-item>
+              <el-menu-item index="3-2" :route="{path: '/teacher'}">学生列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">短学期管理</template>
+            <el-menu-item-group>
+              <template slot="title">学生管理</template>
+            <el-menu-item index="4-1" :route="{path: '/home'}">导入学生信息</el-menu-item>
+              <el-menu-item index="4-2" :route="{path: '/teacher'}">学生列表</el-menu-item>
             </el-menu-item-group>
           </el-submenu>
         </el-menu>
+
       </div>
 
       <!-- {{baseInfo}} -->
--- src/view/teacher/teacher.vue
+++ src/view/teacher/teacher.vue
@@ -1,43 +1,71 @@
 <template lang="html">
   <div class="">
     <div style="margin-bottom:10px;padding:20px;">
-      <h2 style="margin-top:0">活动详情</h2>
+      <h2 style="margin-top:0">学生信息</h2>
       <el-breadcrumb separator="/">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        <el-breadcrumb-item>学生管理</el-breadcrumb-item>
+        <el-breadcrumb-item>学生列表</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div style="background-color:#fff;padding:20px;margin:18px;margin-top:0">
-      <el-form :model="form" >
-        <el-row class="search">
-          <el-col :span="6">
-            <el-form-item label="用户名">
-              <el-input v-model="form.user" placeholder="审批人"></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="6">
-            <el-form-item label="学号">
-              <el-input v-model="form.user" placeholder="审批人"></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="6">
-            <el-form-item label="班级">
-              <el-select v-model="value5" multiple placeholder="请选择">
-               <el-option
-                 v-for="item in options"
-                 :label="item.label"
-                 :value="item.value">
-               </el-option>
-             </el-select>
-            </el-form-item>
-          </el-col>
-          <el-col :span="6">
-            <el-button type="primary">搜索</el-button>
-          </el-col>
-        </el-row>
-      </el-form>
+      <!-- <el-collapse v-model="activeName" accordion> -->
+        <!-- <el-collapse-item title="搜索" name="1"> -->
+          <el-form :model="form" >
+            <el-row class="search">
+              <el-col :span="8">
+                <el-form-item label="学号">
+                  <el-input v-model="form.user"></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="姓名">
+                  <el-input v-model="form.user"></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="班级">
+                  <el-cascader
+                    :options="options"
+                    v-model="selectedOptions"
+                    @change="handleChange">
+                  </el-cascader>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="实习状态">
+                  <el-select v-model="form.status" clearable placeholder="请选择">
+                    <!-- <el-option
+                      v-for="item in options"
+                      :label="item.label"
+                      :value="item.value">
+                    </el-option> -->
+                    <el-option
+                      label="实习中"
+                      value="0">
+                    </el-option>
+                    <el-option
+                      label="未实习"
+                      value="1">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <div style="color:#fff">1</div>
+              </el-col>
+              <el-col :span="8">
+                <el-button type="primary">搜索</el-button>
+              </el-col>
+            </el-row>
+          </el-form>
+          <span style="font-size:14px;color:#666;text-indent:10px;">快捷搜索：</span>
+          <el-button type="text">全部</el-button>
+          <el-button type="text">毕业班</el-button>
+          <el-button type="text">非毕业班</el-button>
+        <!-- </el-collapse-item> -->
+      <!-- </el-collapse> -->
+
       <el-table
        :data="tableData"
        border
@@ -45,13 +73,9 @@
        style="width: 100%"
        @selection-change="handleSelectionChange">
        <el-table-column
-         type="selection"
-         width="55">
-       </el-table-column>
-       <el-table-column
-         label="日期"
+         prop="num"
+         label="学号"
          width="120">
-         <template scope="scope">{{ scope.row.date }}</template>
        </el-table-column>
        <el-table-column
          prop="name"
@@ -59,11 +83,43 @@
          width="120">
        </el-table-column>
        <el-table-column
+         prop="grade"
+         label="年级"
+         width="80"
+         show-overflow-tooltip>
+       </el-table-column>
+       <el-table-column
+         prop="pro"
+         label="专业"
+         width="120"
+         show-overflow-tooltip>
+       </el-table-column>
+       <el-table-column
+         prop="class"
+         label="班级"
+         width="80"
+         show-overflow-tooltip>
+       </el-table-column>
+       <el-table-column
+         prop="status"
+         label="实习状态"
+         width="100"
+         show-overflow-tooltip>
+       </el-table-column>
+       <el-table-column
          prop="address"
-         label="地址"
+         label="实习公司"
          show-overflow-tooltip>
        </el-table-column>
      </el-table>
+     <el-pagination
+       @size-change="handleSizeChange"
+       @current-change="handleCurrentChange"
+       :current-page.sync="currentPage"
+       :page-size="10"
+       layout="prev, pager, next"
+       :page-count="20">
+     </el-pagination>
    </div>
   </div>
 </template>
@@ -72,43 +128,93 @@
 export default {
   data() {
     return {
-      form:{
-        user: ''
+      currentPage: 1,
+      form: {
+        user: '',
+        status: '',
       },
       tableData: [{
-         date: '2016-05-02',
-         name: '王小虎',
-         address: '上海市普陀区金沙江路 1518 弄'
-       }, {
-         date: '2016-05-04',
-         name: '王小虎',
-         address: '上海市普陀区金沙江路 1517 弄'
-       }, {
-         date: '2016-05-01',
-         name: '王小虎',
-         address: '上海市普陀区金沙江路 1519 弄'
-       }, {
-         date: '2016-05-03',
-         name: '王小虎',
-         address: '上海市普陀区金沙江路 1516 弄'
-       }],
-       options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }, {
-          value: '选项4',
-          label: '龙须面'
-        }, {
-          value: '选项5',
-          label: '北京烤鸭'
-        }],
-        value5: []
+        num: '130708122',
+        name: '王虎',
+        grade: '13级',
+        pro: '网络工程',
+        class: '1班',
+        status: '实习中',
+        address: '杭州XX网络技术有限公司'
+      }, {
+        num: '130708124',
+        name: '张菁菁',
+        grade: '13级',
+        pro: '网络工程',
+        class: '1班',
+        status: '未实习',
+        address: '/'
+      }, {
+        num: '130708125',
+        name: '黄正',
+        grade: '13级',
+        pro: '网络工程',
+        class: '1班',
+        status: '实习中',
+        address: '上海XX文化传播有限公司'
+      }, {
+        num: '130708126',
+        name: '李莉',
+        grade: '13级',
+        pro: '网络工程',
+        class: '1班',
+        status: '实习中',
+        address: '浙江广播电视集团'
+      }],
+      options: [{
+          value: '13',
+          label: '13级',
+          children: [{
+              value: 'shejiyuanze',
+              label: '网络工程',
+              children: [{
+                  value: 'yiban',
+                  label: '1班'
+                },
+                {
+                  value: 'erban',
+                  label: '2班'
+                }
+              ]
+            },
+            {
+              value: 'daohang',
+              label: '信息系统管理与设计',
+              children: [{
+                value: 'all',
+                label: '全部'
+              }]
+            }
+          ]
+        },
+        {
+          value: '14',
+          label: '14级'
+        },
+        {
+          value: '15',
+          label: '15级'
+        },
+        {
+          value: '16',
+          label: '16级'
+        }
+      ],
+      value5: []
+    }
+  },
+
+ methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
     }
   }
 }
